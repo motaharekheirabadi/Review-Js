7f0bd285dfd3b4cd0d05e68af863b23b3961a644
motaharekheirabadi <92826072+motaharekheirabadi@users.noreply.github.com>
2022-07-23 17:18:43 +0430

    chenge file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-    
+    //! if statement
+
+    const age = 10;
+    if(age >= 18){
+        console.log("you are good to go");
+    }else if (age <= 15){
+        console.log("you are realy yung" )
+    }else{
+        console.log("you are not old enough" )
+    }
+
+
